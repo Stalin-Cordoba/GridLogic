b542bf4da90d58332da33c645b06001bb23a9bd7
Stalin-Cordoba <wisquiquikiri13@gmail.com>
2026-06-24 23:17:20 -0600

    Base de datos creada para la aplicación

---
+++ app/src/main/java/com/example/gridlogicprototipo/ui/dao/AppDatabase.kt
@@ -0,0 +1,36 @@
+package com.example.gridlogicprototipo.ui.dao
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.example.gridlogicprototipo.ui.room_models.Exercise
+
+@Database(entities = [Exercise::class], version = 1, exportSchema = false)
+abstract class AppDatabase : RoomDatabase() {
+
+    // Definimos el acceso al DAO
+    abstract fun exerciseDao(): ExerciseDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: AppDatabase? = null
+
+        // Patrón Singleton para evitar abrir múltiples instancias de la base de datos
+        fun getDatabase(context: Context): AppDatabase {
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    AppDatabase::class.java,
+                    "gridlogic_database" // El nombre físico del archivo de la BD en el teléfono
+                )
+                    // Opcional: permite recrear la BD si cambias la versión sin migración
+                    .fallbackToDestructiveMigration()
+                    .build()
+
+                INSTANCE = instance
+                instance
+            }
+        }
+    }
+}
